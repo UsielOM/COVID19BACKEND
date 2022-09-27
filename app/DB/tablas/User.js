de9cdb709a26cf511961448df441bea53971a2d5
UsielOM <usiel31197@outlook.es>
2022-09-26 21:57:27 -0500

    Tabla User modelo

---
+++ app/DB/tablas/User.js
@@ -0,0 +1,39 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../conexionBD');
+
+const User = sequelize.define('User', {
+
+    idUser: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    FirstName: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    LastName: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+
+    Email: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+
+    Phone: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false
+    },
+    Password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    }
+
+}, {
+    freezeTableName: true,
+    timestamps: false,
+});
+
+module.exports = User
