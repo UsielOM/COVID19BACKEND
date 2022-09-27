f05e58f7123ae1d5f61d4135ccb9da587305232a
UsielOM <usiel31197@outlook.es>
2022-09-26 21:44:23 -0500

    Comunicacion con bd

--- app/DB/mariadb.js
+++ app/DB/mariadb.js
@@ -3,7 +3,7 @@ const sequelize = require('./conexionBD');
 
 init = function() {
 
-    sequelize.authentificate().then(() => {
+    sequelize.authenticate().then(() => {
         console.log("Conexion establecida exitosamente con mariadb");
     }).catch(err => {
         console.error("Conexion no establecida", err);
