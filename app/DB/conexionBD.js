064ea343088d2f2e283b09e32b3c2f9b99b8e426
UsielOM <usiel31197@outlook.es>
2022-12-09 12:02:38 -0600

    Modificaciones

--- app/DB/conexionBD.js
+++ app/DB/conexionBD.js
@@ -1,15 +1,15 @@
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize("ProyectoCovid", "sa", "021158", {
-//     host: 'localhost',
-//     dialect: "mssql",
-//     port: 1433
-// });
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize("ProyectoCovid", "sa", "021158", {
+    host: 'localhost',
+    dialect: "mssql",
+    port: 1433
+});
 // module.exports = sequelize;
 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize("backendcovid", "root", "sasa", {
-    host: 'localhost',
-    dialect: "mariadb",
-    port: 3306
-})
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize("backendcovid", "root", "sasa", {
+//     host: 'localhost',
+//     dialect: "mariadb",
+//     port: 3306
+// })
 module.exports = sequelize;
