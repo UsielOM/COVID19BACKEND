57e8a9ccc62a05211c744f73967a9650149b0a87
UsielOM <usiel31197@outlook.es>
2023-01-17 20:36:38 -0600

    Creacion y exportacion del archivo postSurveyAnswers

---
+++ app/Routers/postSurveyAnswersRouters.js
@@ -0,0 +1,10 @@
+odule.exports = function(app, mariadb) {
+
+
+    app.post('/post/postSurveyAnswers', (req, res) => {
+        mariadb.postSurveyAnswers(req.body, function(result) {
+            res.send(result);
+        })
+    })
+
+}
